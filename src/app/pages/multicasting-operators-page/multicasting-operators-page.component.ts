0d21200156662830aa226ede008366560a72d9a1
vaalgrind <paweltomkowski@gmail.com>
2022-05-20 11:08:03 +0200

    Multicasting operators.

--- src/app/pages/multicasting-operators-page/multicasting-operators-page.component.ts
+++ src/app/pages/multicasting-operators-page/multicasting-operators-page.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { map, share, shareReplay, Subject, tap } from 'rxjs';
+import { LoggingService } from 'src/app/logging.service';
 
 @Component({
   selector: 'app-multicasting-operators-page',
   templateUrl: './multicasting-operators-page.component.html',
-  styleUrls: ['./multicasting-operators-page.component.scss']
+  styleUrls: ['./multicasting-operators-page.component.scss'],
 })
 export class MulticastingOperatorsPageComponent implements OnInit {
+  multicast$ = new Subject();
+  obsA$ = this.multicast$.asObservable().pipe(
+    map(() => 'A' + this.obsAResult.length),
+    tap((value) => {
+      this.loggingService.addLog('Tap', value);
+      this.obsAResult.push(value);
+    }),
+    share()
+  );
+  obsAResult = [];
 
-  constructor() { }
+  multicastReplay$ = new Subject();
+  obsB$ = this.multicastReplay$.asObservable().pipe(
+    map(() => 'B' + this.obsAResult.length),
+    tap((value) => {
+      this.loggingService.addLog('Tap', value);
+      this.obsAResult.push(value);
+    }),
+    shareReplay(1)
+  );
+  obsBResult = [];
 
-  ngOnInit(): void {
-  }
+  showLateSubscriber = false;
 
+  constructor(private readonly loggingService: LoggingService) {}
+
+  ngOnInit(): void {}
 }
